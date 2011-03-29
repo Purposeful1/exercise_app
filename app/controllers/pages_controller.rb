1274181695e41d05663b55792f441c15adb9fb84
Purposeful1 <pds@u.northwestern.edu>
2011-03-29 06:01:30 -0500

    Worked on static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,32 +1,42 @@
 class PagesController < ApplicationController
   def welcome
+    @title = "Welcome"
   end
 
   def blog
+    @title = "Our Blog"
   end
 
   def about
+    @title = "About Us"
   end
 
   def contact
+    @title = "Contact Us"
   end
 
   def signup
+    @title = "Sign Up"
   end
 
   def login
+    @title = "Log in"
   end
 
   def team
+    @title = "My Team"
   end
 
   def progress
+    @title = "Progress"
   end
 
   def friends
+    @title = "Friends"
   end
 
   def profile
+    @title = "My Profile"
   end
 
 end
